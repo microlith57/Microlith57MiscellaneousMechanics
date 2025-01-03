b0511c7df3801420750b9d176a4fa9b52131f7b6
microlith57 <microlith57@gmail.com>
2025-01-04 11:31:46 +1300

    implement bidirectional position tracker

--- Source/Entities/TriggersAndRegions/PositionTrackerRegion.cs
+++ Source/Entities/TriggersAndRegions/PositionTrackerRegion.cs
@@ -46,6 +46,13 @@ public sealed class PositionTrackerRegion : Entity {
         CenterLeft,
         CenterRight,
         Size,
+        Speed,
+    }
+
+    private enum DirectionalityType {
+        EntityToSliders,
+        SlidersToEntity,
+        AddDeltas,
     }
 
     private readonly ConditionSource Condition;
@@ -55,12 +62,25 @@ public sealed class PositionTrackerRegion : Entity {
     private DetectionType Detection;
     private StickinessType Stickiness;
     private TrackingType Tracking;
+    private DirectionalityType Directionality;
 
-    private Entity? Target;
+    private Entity? _Target;
     private bool EverTargetted = false;
+
+    private Entity? Target {
+        get => _Target;
+        set {
+            if (_Target == value) return;
+            if (value != null) EverTargetted = true;
+
+            _Target = value;
+            LastValue = null;
+        }
+    }
 
     private Session.Slider SliderX, SliderY;
     private string TargettingFlag;
+    private Vector2? LastValue;
 
     #endregion State
     #region --- Init ---
@@ -80,6 +100,7 @@ public sealed class PositionTrackerRegion : Entity {
         Detection = data.Enum("detection", DetectionType.Intersecting);
         Stickiness = data.Enum("stickiness", StickinessType.Lifelink);
         Tracking = data.Enum("tracking", TrackingType.Position);
+        Directionality = data.Enum("directionality", DirectionalityType.EntityToSliders);
 
         SliderX = sliderX;
         SliderY = sliderY;
@@ -109,19 +130,45 @@ public sealed class PositionTrackerRegion : Entity {
     public override void Awake(Scene scene) {
         base.Awake(scene);
         UpdateTarget();
-        UpdateOutputs();
+        UpdateLink(DirectionalityType.EntityToSliders);
     }
 
     public override void Update() {
         base.Update();
         UpdateTarget();
-        UpdateOutputs();
+        UpdateLink(Directionality);
     }
 
-    private void UpdateOutputs() {
-        if (Value is Vector2 vec) {
-            SliderX.Value = vec.X;
-            SliderY.Value = vec.Y;
+    private void UpdateLink(DirectionalityType bidi) {
+        Vector2? position;
+
+        switch (bidi) {
+            case DirectionalityType.SlidersToEntity:
+                position = new(SliderX.Value, SliderY.Value);
+                break;
+
+            case DirectionalityType.AddDeltas:
+                if (LastValue != null) {
+                    position = LastValue.Value
+                             + LastValue.Value - new Vector2(SliderX.Value, SliderY.Value)
+                             + LastValue.Value - Value;
+                } else
+                    position = Value;
+                break;
+
+            default:
+                position = Value;
+                break;
+        }
+
+        if (position is Vector2 vec) {
+            if (bidi != DirectionalityType.EntityToSliders)
+                Value = position;
+
+            if (bidi != DirectionalityType.SlidersToEntity) {
+                SliderX.Value = vec.X;
+                SliderY.Value = vec.Y;
+            }
         }
 
         if (Scene is Level level && TargettingFlag != "")
@@ -140,6 +187,45 @@ public sealed class PositionTrackerRegion : Entity {
                 case TrackingType.CenterRight: return Target.CenterRight;
                 case TrackingType.Size:
                     return new(Target.Collider?.Width ?? 0f, Target.Collider?.Height ?? 0f);
+                case TrackingType.Speed:
+                    if (Target is Player player)
+                        return player.Speed;
+                    else if (Target.Get<Holdable>() is Holdable hold)
+                        return hold.GetSpeed();
+
+                    return null;
+
+                default: throw new Exception("unreachable");
+            }
+        }
+        set {
+            if (Target == null || value == null) return;
+            switch (Tracking) {
+                case TrackingType.Position:
+                    Target.Position = value.Value; return;
+                case TrackingType.Center:
+                    Target.Center = value.Value; return;
+                case TrackingType.TopCenter:
+                    Target.TopCenter = value.Value; return;
+                case TrackingType.BottomCenter:
+                    Target.BottomCenter = value.Value; return;
+                case TrackingType.CenterLeft:
+                    Target.CenterLeft = value.Value; return;
+                case TrackingType.CenterRight:
+                    Target.CenterRight = value.Value; return;
+                case TrackingType.Size:
+                    if (Target.Collider != null) {
+                        Target.Collider.Width = value.Value.X;
+                        Target.Collider.Height = value.Value.Y;
+                    }
+                    return;
+                case TrackingType.Speed:
+                    if (Target is Player player)
+                        player.Speed = value.Value;
+                    else if (Target.Get<Holdable>() is Holdable hold)
+                        hold.SetSpeed(value.Value);
+
+                    return;
                 default: throw new Exception("unreachable");
             }
         }
@@ -189,8 +275,6 @@ public sealed class PositionTrackerRegion : Entity {
             default: throw new Exception("unreachable");
         }
         Collidable = false;
-
-        EverTargetted |= Target != null;
     }
 
     private bool TryGetTarget(ref Entity? target)
