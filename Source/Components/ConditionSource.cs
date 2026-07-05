0a91f30848e420091c92f210dd62933c69725aa8
microlith57 <microlith57@gmail.com>
2026-07-05 13:59:47 +1200

    conditionsource.from

--- Source/Components/ConditionSource.cs
+++ Source/Components/ConditionSource.cs
@@ -1,7 +1,6 @@
 namespace Celeste.Mod.Microlith57Misc.Components;
 
 public class ConditionSource(bool invert = false) : Component(active: false, visible: false) {
-
     public virtual bool? RawValue => null;
 
     public bool Default = false;
@@ -9,15 +8,21 @@ public class ConditionSource(bool invert = false) : Component(active: false, vis
     public bool Value => RawValue.HasValue ? RawValue.Value ^ Invert : Default;
 
     public class Function(Func<bool> func, bool invert = false) : ConditionSource(invert) {
-
         private readonly Func<bool> _Func = func;
         public override bool? RawValue => _Func() ^ Invert;
-
     }
 
-    public class Flag(string flag, bool invert = false) : ConditionSource(invert) {
+    public class Flag : ConditionSource {
+        public readonly string _Flag;
 
-        public readonly string _Flag = flag;
+        public Flag(string flag, bool invert = false) : base(invert) {
+            flag = flag.Trim();
+            while (flag.StartsWith('!')) {
+                flag = flag.Remove(0, 1).TrimStart();
+                Invert = !Invert;
+            }
+            _Flag = flag;
+        }
 
         public Flag(
             EntityData data,
@@ -30,14 +35,12 @@ public class ConditionSource(bool invert = false) : Component(active: false, vis
         ) {}
 
         public override bool? RawValue =>
-            (_Flag == "" || Scene is not Level level)
+            (string.IsNullOrEmpty(_Flag) || Scene is not Level level)
                 ? null
                 : level.Session.GetFlag(_Flag);
-
     }
 
     public class Expr : ConditionSource {
-
         private readonly object? _Expr;
 
         public Expr(
@@ -62,7 +65,29 @@ public class ConditionSource(bool invert = false) : Component(active: false, vis
             (_Expr == null || Scene is not Level level)
                 ? null
                 : Imports.FrostHelper.GetBoolSessionExpressionValue(_Expr, level.Session);
-
     }
 
+    public static ConditionSource From(
+        FlagOrExpr conditionType,
+        EntityData data,
+        string namePrefix = "",
+        string? name = null,
+        string? legacyInvertName = "invertFlag",
+        string ifAbsent = "",
+        bool invert = false,
+        bool @default = false
+    ) {
+        string FormatName(string suffix) {
+            if (name is not null) return name;
+            if (string.IsNullOrEmpty(namePrefix)) return suffix;
+            return name + suffix.Substring(0, 1).ToUpperInvariant() + suffix.Substring(1);
+        }
+
+        switch (conditionType) {
+            case FlagOrExpr.Flag:
+                return new Flag(data, FormatName("flag"), ifAbsent, legacyInvertName) {Invert = invert, Default = @default};
+            case FlagOrExpr.Expr:
+                return new Expr(data, FormatName("expression"), ifAbsent) {Invert = invert, Default = @default};
+        }
+    }
 }
